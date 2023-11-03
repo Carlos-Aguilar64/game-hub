08065e10867a097c072f9f2f8851e4972794097d
Carlos Aguilar <carlosa@Carloss-iMac-2.local>
2023-11-03 14:55:30 -0700

    Filtering by Genre

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -3,8 +3,11 @@ import useData from "./useData";
 export interface Genre {
   id: number;
   name: string;
+  image_background: string;
 }
 
-const useGenres = () => useData<Genre>("/genres");
+const useGenres = (onSelectedGenre: (genre: Genre) => void | null) =>
+  useData<Genre>("/genres");
+// const useGenres = (onSelecteGenre: Genre | null) => useData<Genre>("/genres");
 
 export default useGenres;
