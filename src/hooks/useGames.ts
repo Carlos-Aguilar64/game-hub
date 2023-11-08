94d2224f129d579ac7b054aa1ef154354346a6e4
Carlos Aguilar <carlosa@Carloss-iMac-2.local>
2023-11-08 14:38:06 -0800

    Filter games by Platform

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,5 +1,6 @@
 import useData from "./useData";
 import { Genre } from "./useGenres";
+import usePlatforms from "./usePlatforms";
 
 export interface Platform {
   id: number;
@@ -15,10 +16,15 @@ export interface Game {
   metacritic: number;
 }
 
-const useGames = (selectedGenre: Genre | null) =>
-  useData<Game>("/games", { params: { genres: selectedGenre?.id } }, [
-    selectedGenre?.id,
-  ]);
+const useGames = (
+  selectedGenre: Genre | null,
+  selectedPlaform: Platform | null
+) =>
+  useData<Game>(
+    "/games",
+    { params: { genres: selectedGenre?.id, platforms: selectedPlaform?.id } },
+    [selectedGenre?.id, selectedPlaform?.id]
+  );
 
 export default useGames;
 
