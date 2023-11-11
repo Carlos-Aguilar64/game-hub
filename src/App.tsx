428be959e8d916acc4730eacebfc8b7f0806fd2c
Carlos Aguilar <carlosa@Carloss-iMac-2.local>
2023-11-10 17:52:17 -0800

    Implement Search

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,7 @@ export interface GameQuery {
   genre: Genre | null;
   platform: Platform | null;
   sortOrder: string;
+  searchTex: string;
 }
 
 function App() {
@@ -29,7 +30,9 @@ function App() {
       }}
     >
       <GridItem area="nav">
-        <NavBar />
+        <NavBar
+          onSearch={(searchTex) => setGameQuery({ ...gameQuery, searchTex })}
+        />
       </GridItem>
       <Show above="lg">
         <GridItem area="aside" paddingX={5}>
