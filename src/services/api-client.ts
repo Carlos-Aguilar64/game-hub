1d14fb934424cac4b0eedc2951a1308f7e147f2e
Carlos <carlosa@taqueriamovil.com>
2023-11-19 10:43:02 -0800

    Put key back in code

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,7 +3,8 @@ import axios from "axios";
 const instance = axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
-    key: import.meta.env.VITE_REACT_APP_API_KEY as string,
+    // key: import.meta.env.VITE_REACT_APP_API_KEY as string,
+    key: "bb16b6830c0748649f9b7d867beae5c8",
   },
 });
 
